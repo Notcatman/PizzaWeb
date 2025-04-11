b5934b8e2c72065a0fbba7848c6dbe86bfe5baeb
Notcatman <15.03.dato.dato@gmail.com>
2025-04-11 16:45:08 +0400

    Update

--- forms/Flaskform.py
+++ forms/Flaskform.py
@@ -1,6 +1,5 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, SelectField, DateField
-from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 
 class RegistrForm(FlaskForm):
